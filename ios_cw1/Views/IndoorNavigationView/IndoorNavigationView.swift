f979e7f15bde0e735ca8f56f820930c895be49bb
OKSamarasinghe <oshadakaveensamarasinghe@gmail.com>
2026-03-05 08:46:05 +0530

    Feat: Created IndoorNavigationView file

---
+++ ios_cw1/Views/IndoorNavigationView/IndoorNavigationView.swift
@@ -0,0 +1,15 @@
+//
+//  Untitled.swift
+//  ios_cw1
+//
+//  Created by Oshadha Samarasinghe on 2026-03-05.
+//
+
+import SwiftUI
+
+struct IndoorNavigationView: View {
+    
+    var body: some View {
+        
+    }
+}
